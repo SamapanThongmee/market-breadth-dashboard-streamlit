e08d034c6cab67560fcfbb0a212b3b1dab608811
Samapan Thongmee <83812108+SamapanThongmee@users.noreply.github.com>
2025-03-07 01:37:42 +0700

    Update main.py

--- main.py
+++ main.py
@@ -196,7 +196,7 @@ st.markdown(
     '''
     <p style="font-size:12px;">
     <span style="color:#69DC5B;"><b>McClellan Oscillator</b></span> represents the difference between the 10% and 5% exponential moving averages (EMAs) of daily market breadth, indicating short-term market momentum and overbought/oversold conditions.<br>
-    <span style="color:#27AE60;"><b>McClellan Summation Index</b></span> represents the cumulative sum of the McClellan Oscillator, used for intermediate- to long-term analysis of market direction and strength.
+    <span style="color:#27AE60;"><b>McClellan Summation Index</b></span> represents the cumulative sum of the McClellan Oscillator, used for intermediate to long-term analysis of market direction and strength.
     </p>
     ''', 
     unsafe_allow_html=True
